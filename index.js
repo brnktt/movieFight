93641e7ae99f6de17e722146e566d5b59fc72210
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-04 14:35:47 +0100

    Configuration of OMDB API request

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "7739540f",
+      i: "tt0848228"
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
