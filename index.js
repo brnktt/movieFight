3a2318a3768eb5deb72178837d747c751b2f7421
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-04 23:43:58 +0100

    Finished results menu generation

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const root = document.querySelector(".autocomplete");
 root.innerHTML = `
   <label><b>Search For a Movie</b></label>
   <input class="input" />
-  <div class="dropdown is-active">
+  <div class="dropdown">
     <div class="dropdown-menu">
       <div class="dropdown-content results">
 
@@ -33,16 +33,32 @@ const resultsWrapper = document.querySelector(".results");
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
 
-  for (let movie of movies) {
-    const div = document.createElement("div");
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
 
-    div.innerHTML = `
-      <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
     `;
 
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
-input.addEventListener("input", debounce(onInput, 500));
+input.addEventListener("input", debounce(onInput, 1000));
+
+document.addEventListener("click", event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
