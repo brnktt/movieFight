ddd1e54be0b032d93594f29a6b96b8edad145878
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-04 22:43:20 +0100

    Added base scturcture for results menu

--- index.js
+++ index.js
@@ -6,13 +6,43 @@ const fetchData = async searchTerm => {
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
-const input = document.querySelector("input");
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown is-active">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
 
-const onInput = event => {
-  fetchData(event.target.value);
+      </div>
+    </div>
+  </div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
